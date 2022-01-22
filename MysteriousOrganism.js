f3da86649c841b22c510604dd15783c768d04dd5
Sasha <s.marchuk95@gmail.com>
2022-01-22 12:35:05 +0100

    need to have a closer look

---
+++ MysteriousOrganism.js
@@ -0,0 +1,55 @@
+// Returns a random DNA base
+const returnRandBase = () => {
+  const dnaBases = ['A', 'T', 'C', 'G'];
+  return dnaBases[Math.floor(Math.random() * 4)];
+};
+
+// Returns a random single stand of DNA containing 15 bases
+const mockUpStrand = () => {
+  const newStrand = [];
+  for (let i = 0; i < 15; i++) {
+    newStrand.push(returnRandBase());
+  }
+  return newStrand;
+};
+
+const pAequorFactory = (specimenNum, dna ) => {
+  return {
+    specimenNum,
+    dna,
+    mutate() {
+      const randIndex = Math.floor(Math.random() * this.dna.length);
+      let newBase = returnRandBase();
+      while (this.dna[randIndex] === newBase) {
+      newBase = returnRandBase();
+    } 
+    this.dna[randIndex] = newBase;
+    return this.dna;
+    },
+    compareDNA(pAequor) {
+      let count = 0
+      for(let i = 0; i < this.dna.length; i++){
+        if (this.dna[i] === pAequor.dna[i]) {
+          count += 1
+        }
+      }
+      console.log(`Species have ${Math.floor(100 / 15 * count)} in common`)
+    }, 
+    willLikelySurvive() {
+      const cOrG = this.dna.filter(el => el === "C" || el === "G");
+      return cOrG.length / this.dna.length >= 0.6;
+    },
+  }
+}
+const survivingSpecimen = [];
+let idCounter = 1;
+
+while (survivingSpecimen.length < 30) {
+  let newOrg = pAequorFactory(idCounter, mockUpStrand());
+  if (newOrg.willLikelySurvive()) {
+    survivingSpecimen.push(newOrg);
+  }
+  idCounter++;
+}
+
+console.log(survivingSpecimen)
